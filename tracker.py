43f6edc55f283836cb96102a6bc47ab0ea0f4bb9
Rodondi <133044105+Rodondi@users.noreply.github.com>
2025-04-27 00:02:08 +0300

    code review and edits

--- tracker.py
+++ tracker.py
@@ -1,18 +1,20 @@
-from api import calculate_transport_emission, calculate_energy_emission
 import sqlite3
 from datetime import datetime
+from api import calculate_transport_emission, calculate_energy_emission
 
 def log_activities(transport_km, electricity_kwh, diet, vehicle_model_id=None):
-    """Logs an activity to the database."""
+    """Log a user's daily activities into the database."""
     transport_emission = calculate_transport_emission(transport_km, vehicle_model_id)
     electricity_emission = calculate_energy_emission(electricity_kwh)
 
     conn = sqlite3.connect("data/eco_tracker.db")
     cursor = conn.cursor()
+
     cursor.execute("""
         INSERT INTO logs (date, transport_km, electricity_kwh, diet)
         VALUES (?, ?, ?, ?)
     """, (datetime.now().isoformat(), transport_km, electricity_kwh, diet))
+
     conn.commit()
     conn.close()
 
